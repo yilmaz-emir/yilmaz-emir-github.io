a2c7157eee6dc333a20658708e09dc09a1d9bc48
yilmaz-emir <215802058+yilmaz-emir@users.noreply.github.com>
2025-09-25 22:40:01 +0000

    Deploying to gh-pages from @ yilmaz-emir/yilmaz-emir.github.io@624c4270d5f405cbf4aa47e227cd45455550e834 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,23 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
