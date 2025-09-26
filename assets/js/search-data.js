1038280866ee446f1ef2f3549e5778a784b5f32c
yilmaz-emir <215802058+yilmaz-emir@users.noreply.github.com>
2025-09-26 07:18:49 +0000

    Deploying to gh-pages from @ yilmaz-emir/yilmaz-emir.github.io@507766790e8d8b10589b572682bb45c7d987940d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "publications",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
-        },{id: "nav-teaching",
+  },{id: "nav-teaching",
           title: "teaching",
           description: "",
           section: "Navigation",
